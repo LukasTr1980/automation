294b38a6e8c123614056cd24cb6c7be5e1dc5ef6
LukasTr1980 <lukas.troebinger@gmail.com>
2024-01-07 10:53:33 +0100

    In `TimeDisplay` added also the year

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased] - 2024-01-01
+### Added
+- In `TimeDisplay` added also the year
+
 ## [v15.11.0] - 2023-12-31
 ### Added
 - `TimeDisplay` on the right side of `NavMenu`
--- viteclientts/src/components/TimeDisplay.tsx
+++ viteclientts/src/components/TimeDisplay.tsx
@@ -16,8 +16,9 @@ const TimeDisplay: React.FC = () => {
     const dayOfWeek = date.toLocaleString('de-DE', { weekday: 'short' }); // German formatting for day
     const day = date.getDate();
     const month = date.toLocaleString('de-DE', { month: 'short' });
+    const year = date.getFullYear();
     const time = date.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
-    const formattedDate = `${dayOfWeek}, ${day}. ${month}, ${time}`;
+    const formattedDate = `${dayOfWeek}, ${day}. ${month} ${year}, ${time}`;
     return formattedDate;
   };
 
