4f3572a5f84d9e930a5f75443f21aa656d1e3db8
LukasTr1980 <lukas.troebinger@gmail.com>
2024-02-25 11:05:07 +0100

    - PWA support and configurations for the app.

--- viteclientts/vite.config.ts
+++ viteclientts/vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA()
+  ],
   server: {
     cors: {
       origin: ['http://localhost:5173', 'https://automation.charts.cx', 'http://localhost:8523', 'https://charts.cx'],
